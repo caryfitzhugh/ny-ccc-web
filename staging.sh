4c2fbafeedc626f5bd8e961b99fd3a05dec82ecf
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-12-22 12:27:38 -0500

    update the staging script

--- staging.sh
+++ staging.sh
@@ -1,6 +1,6 @@
 rm -rf build/
 npm run build
 
-aws s3 sync --profile=nescaum build/ s3://ma-web-nescaum-ccsc-dataservices --acl public-read
-aws cloudfront create-invalidation --profile=nescaum --distribution-id E19XY2FPLOWT0A --paths "/*"
+aws s3 sync --profile=nescaum build/ s3://ny-web-nescaum-ccsc-dataservices --acl public-read
 rm -rf build/
+echo "Page deployed to: http://ny-web-nescaum-ccsc-dataservices.s3-website-us-east-1.amazonaws.com"
