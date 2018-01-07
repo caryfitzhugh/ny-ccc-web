202622bf23cfc9c71e441b2ebf5ba00dacdce000
Chris Diego <chrisdiego1@gmail.com>
2018-01-06 23:34:38 -0500

    content removal

--- src/sectors/economy.js
+++ src/sectors/economy.js
@@ -25,86 +25,40 @@ class EconomySectorPage extends Component {
         image_credit="John Phelan"
         showcased_resources={showcased_resources}
         footnotes={[
-          <span>Office of Governor Charlie Baker, 2015, Letter to Request Federal Emergency Declaration – Attachment A:  2015 Severe Winter Weather Pattern Impacts - Supplemental Information. <a href='http://www.mass.gov/governor/docs/news/attachment-a-severe-winter-weather-pattern-impacts-supplemental-info.pdf'>http://www.mass.gov/governor/docs/news/attachment-a-severe-winter-weather-pattern-impacts-supplemental-info.pdf </a></span>,
-          <span> IHS Global Insights, 2015, The Economic Costs of Disruption from a Snowtstorm. <a href='https://timedotcom.files.wordpress.com/2015/01/economic-costs-of-snowstorms.pdf'>https://timedotcom.files.wordpress.com/2015/01/economic-costs-of-snowstorms.pdf</a></span>,
-          <span> NOAA, 2015, Fisheries Economics of the United States. <a href='http://www.nmfs.noaa.gov/stories/2017/04/docs/feus2015_fs_final3_web.pdf'>http://www.nmfs.noaa.gov/stories/2017/04/docs/feus2015_fs_final3_web.pdf </a></span>,
-          <span> NOAA, 2014, Gulf of Maine Atlantic Cod: 2014 Assessment Update Report, <a href='https://www.nefsc.noaa.gov/saw/cod/pdfs/GoM_cod_2014_update_20140822.pdf'>https://www.nefsc.noaa.gov/saw/cod/pdfs/GoM_cod_2014_update_20140822.pdf</a></span>,
+          <span>A stormtrooper comes down the ramp of the pirateship and waves to the gantry officer, pointing to his ear indicating his comlink is not working. The gantry officer shakes his head in disgust and heads for the door, giving his aide an annoyed look. </span>,
         ]}
         sections={[
-          {title: "Impacts",
+          {title: "Headline",
             id: 'impacts',
             content: <p> </p>,
             subsections: [
-              { name:<SearchLink params={{query:"economy rising temperatures"}}>Rising Temperatures</SearchLink>,
+              { name:<SearchLink params={{query:"economy rising temperatures"}}>Headline</SearchLink>,
                 id: "impacts_rising_temperatures",
                 content: <div>
-                  <p>Rising temperatures and heat waves will likely cause more sick days due to increases in heat-related illnesses such as heat stroke and asthma, and will also increase air conditioning needs and energy costs for businesses.  Outdoor or factory-based industries such as manufacturing and home construction may also be significantly impacted by days that are too hot for workers to safely complete their tasks.</p>
-                  <p>In 2014, Massachusetts commercial fishermen sold $4.4 billion dollars of fish, directly supporting some 92,000 jobs<Footnote num='3'/>.  Rising estuarine and ocean temperatures may cause declines in certain populations of cold water fish and shellfish and the fishing jobs and other economic activities that require healthy fisheries.  Cod stocks in the Gulf of Maine dropped by 75% between 2004 and 2014<Footnote num='4'/>.  Some fresh-water fish stocks and the tourism they support may also decline.</p>
+                  <p> Take over. We've got a bad transmitter. I'll see what I can do.</p>
                 </div>
               },
-              { name: <SearchLink params={{query:"economy changes in precipitation"}}>Changes in Precipitation</SearchLink>,
+              { name: <SearchLink params={{query:"economy changes in precipitation"}}>Headline</SearchLink>,
                 id: 'impacts_changes_in_precipitation',
                 content: <div>
-                  <p>If water availability is affected by an increased variability in precipitation, water-dependent manufacturing industries will likely be strained, as will agricultural operations. In addition, more municipalities may need to raise water rates due to increasing prevalence of droughts and water shortages.</p>
+                  <p> You know, between his howling and your blasting everything in sight, it's a wonder the whole station doesn't know we're here.</p>
                 </div>
-              },
-              { name: <SearchLink params={{query:"economy extreme weather"}}>Extreme Weather</SearchLink>,
-                id: 'impacts_extreme_weather',
-                content: <div>
-                  <p>Heavier, harsher storms causing floods will likely have an impact on buildings, energy delivery and transmission, and transportation systems. Many historic town centers and manufacturing areas are located in river valleys and flood plains and will be exposed to increased inundation. More frequent and extreme weather events may disrupt the supply chains for businesses dependent on specific fuels or raw materials. Similar impacts may affect product delivery. Extreme weather may also prevent workers from getting to their jobs, slowing the entire economy.</p>
-                  <p>The state’s sizable insurance and financial management sector with its 200,000 jobs may be especially affected by the growing cost of insuring against extreme weather in Massachusetts and across the country.  Steep financial losses for investors caused by climate-related impacts could also affect the state’s investment firms, while gains from new economic opportunities like renewable energy may benefit firms with smart investments in these areas.</p>
-                </div>
-              },
-              { name: <SearchLink params={{query: "economy sea level rise"}}>Sea Level Rise</SearchLink>,
-                id: 'impacts_slr',
-                content: <p>
-                  Much of the Commonwealth’s critical infrastructure, not to mention homes and businesses, lie on the coast and must be prepared for the eventuality of sea-level rise. If not, encroaching salt water and higher incidence of flooding could slow the economy by damaging transportation and utility infrastructure, as well as goods and properties.
-                </p>
               }
             ]},
-            {title:"Actions",
+            {title:"Headline",
               id:'actions',
               content: <p></p>,
               subsections: [{
-                name: <SearchLink params={{query:"economy planning"}}>Planning</SearchLink>,
+                name: <SearchLink params={{query:"economy planning"}}>Headline</SearchLink>,
                 id: 'actions_planning',
                 content: <ul>
-                  <li>Assess the ability to meet health care capacity needs, especially for business operations that are vulnerable to flooding, extreme heat, or poor air quality.</li>
-                  <li>Encourage businesses to take account of climate change impacts in risk assessments and risk minimization strategies, particularly as data are refined to a sub-state level.</li>
-                  <li>Maintain and develop emergency management plans with support from the Massachusetts Emergency Management Agency (MEMA) and the Federal Emergency Management Agency (FEMA).</li>
-                </ul>
-              },
-              {name: <SearchLink params={{query:"economy management practices"}}>Management Practices</SearchLink>,
-                id: 'actions_management_practices',
-                content: <ul>
-                  <li> Avoid siting operations or businesses in flood prone areas. For existing operations in these areas, support building design modifications, such as elevating electrical and HVAC systems above ground level, and installing systems that can withstand frequent flooding and heat waves.</li>
-                  <li> Consider altering operation schedules to cooler times of day when possible.</li>
-                  <li> Support improvements to stormwater management at business facilities to reduce runoff and its water quality impacts, and expand opportunities for water recycling, including grey water.</li>
-                  <li> Examine advantages of increasing inventory supplies and identify alternative materials and inventory suppliers to avoid delivery interruptions.</li>
-                  <li> Investigate alternate transportation routes, energy supplies, and communication systems for suppliers, customers, and workers to respond to evolving climate change impacts.</li>
-                  <li> Provide outreach and educate public and private employers about Occupational Safety and Health Administration (OSHA) requirements regarding employee protections in times of high heat.</li>
-                  <li> Consider using renewable energy in business operations, performing energy efficiency retrofits of facilities, and looking for opportunities to conserve energy in business processes and in employee commuting.</li>
-                </ul>
-              },
-              {name: <SearchLink params={{query:"economy technology"}}>Technology</SearchLink>,
-              id:'actions_technology',
-              content: <ul>
-                <li> Consider improvements to the structural stability, water drainage systems, and weatherproofing of critical infrastructure or other economically vital sites as part of capital improvement projects.</li>
-                <li> Investigate installation of year-round climate control assets, including natural site-specific climate control assets such as tree cover and vegetation, and re-examine the thermal properties of buildings to increase resilience and reduce greenhouse gas production.</li>
-                <li> Seek to reduce water use in energy production by considering alternative technologies, since peak water use in energy production often coincides with periods of high heat and low water availability.</li>
-              </ul>
-              },
-              {name: <SearchLink params={{query:"economy policies laws"}}>Policies/Laws</SearchLink>,
-                id:'actions_policies_laws',
-                content: <ul>
-                  <li>Establish partnerships between industry and government to facilitate technical assistance to businesses that are adapting to climate change.</li>
-                  <li>Encourage or incentivize conversion to more energy-efficient buildings and processes and to local renewable energy sources to reduce dependence on the electric grid and to build resilience for power disruptions that can occur during extreme weather events.</li>
+                  <li> Bring them on! I prefer a straight fight to all this sneaking around.</li>
                 </ul>
               }]},
           {title: "Looking for help?",
             id: 'looking_for_help',
             content: <ul>
-              <li>  Massachusetts Energy and Environmental Affairs <a href='http://www.mass.gov/eea/'>http://www.mass.gov/eea/</a></li>
+              <li> </li>
             </ul>,
             subsections: []
           }
@@ -112,10 +66,7 @@ class EconomySectorPage extends Component {
         ]}
         >
           <div>
-            <p>Massachusetts has a strong and growing economy, backed by a network of 125 colleges and universities that make the Commonwealth a world leader in technology, science, and health care.  In 2017, Massachusetts was ranked the best state by U.S. News and World Report based on its strong economy, excellent health care, and well-performing education system.  </p>
-            <p>Against this vibrant back drop, climate change will produce serious risks as well as new opportunities for many Massachusetts businesses— including businesses in major and growing sectors of the economy such as health care, technology, education, information technology, financial services, manufacturing, and renewable energy.</p>
-            <p>Investment in the development of climate change solutions provide opportunities that the Commonwealth is uniquely suited to pursue given the State’s focus on research and technology.  Massachusetts is already a leader in the transition to a climate-friendly, clean energy economy.  More than 105,000 people are now employed in the clean energy industry—up 75 percent since 2010, and those numbers continue to grow. </p>
-            <p>Climate change will also increase exposure to risks and challenges across every economic sector. Extreme rain and snow storms can cost hundreds of millions of dollars from lost sales, increased fuel and supply prices, and direct damage and clean-up costs. The string of winter storms in February 2015 cost the Commonwealth more than $35 million alone <Footnote num='1'/>, and a full economic shutdown from an extreme weather event would cost approximately $265 million <em>per day</em><Footnote num='1'/>.  </p>
+            <p>As the officer approaches the door, it slides open revealing the towering Chewbacca. The gantry officer, in a momentary state of shock, stumbles backward. With a bone- chilling howl, the giant Wookiee flattens the officer with one blow. The aide immediately reaches for his pistol, but is blasted by Han, dressed as an Imperial stormtrooper. Ben and the robots enter the room quickly followed by Luke, also dressed as a stormtrooper. Luke quickly removes his helmet.  </p>
           </div>
         </SectorDetailLayout>
     );
--- src/sectors/forestry.js
+++ src/sectors/forestry.js
@@ -21,123 +21,59 @@ class ForestrySectorPage extends Component {
       <SectorDetailLayout
         title="Forestry"
         image={sector_image}
-        image_credit="MA Department of Conservation and Recreation"
+        image_credit=""
         showcased_resources={showcased_resources}
         footnotes={[
-          <span> Alerich. C.L. 2000. Forest Statistics for Massachusetts: 1985 and 1998. Research Bulletin NE-148. USDA Forest Service, Northeastern Research Station.     </span>,
-          <span> MassGIS </span>,
-          <span> Campbell, S.M. 2000. Quantifying Public Benefits on Private Forestland in Massachusetts. Massachusetts Forest Stewardship Programs Task Force on Reforming Forest Taxation. </span>,
+          <span> The pirateship is pulled in through port doors of the Death Star, coming to rest in a huge hangar. Thirty stormtroopers stand at attention in a central assembly area.     </span>,
         ]}
         sections={[
-          {title: "Impacts",
+          {title: "Headline",
            id: "impacts",
            content: <p>
-            Some changes in forests as a result of shifting climate patterns may be slow while others may proceed quickly once critical thresholds have been crossed.
+           A line of stormtroopers march toward the pirateship in readiness to board it, while other troopers stand with weapons ready to fire.
            </p>,
            subsections: [
-            {name: <ForestLink changes={['ma::rising temperatures::']}>Rising Temperatures</ForestLink>,
+            {name: <ForestLink changes={['ma::rising temperatures::']}>Headline</ForestLink>,
              id:"impacts_rising_temps",
              content: <div>
-                <p>Warmer temperatures are likely to alter the composition and role of forest species including plants and animals, fungi, and bacteria. They are also likely to cause gradual shifts in the distribution of different tree species.  Hardwoods are likely to recede to higher elevations and spruce-fir may disappear from Massachusetts. Southern forest types such as central, transitional, and southern hardwoods will likely increase in abundance. Changes in sap flow and harvesting seasons for sugar maples could affect maple syrup production.</p>
-                <p>While forest pests are spread by humans, the range for many pests is widened by warming temperatures. Northward expansion of invasive insects that cause disease in particular tree species, such as the hemlock woolly adelgid and southern pine beetle, are likely. The response of other invasives, such as the emerald ash borer, the Asian longhorned beetle, and beech bark disease, is uncertain.</p>
+                <p>Tarkin pushes a button and responds to the intercom buzz.</p>
+                <p>Close all outboard shields! Close all outboard shields!</p>
               </div>
-            },
-            {name: <ForestLink changes={["ma::changes in precipitation::"]}>Changes in Precipitation</ForestLink>,
-             id: 'impacts_changes_in_precipitation',
-             content: <div>
-               <p>Changes in the water cycle caused by wetter winters and springs could increase the negative impacts from invasive species. More frequent and pronounced droughts expected in the Commonwealth could exacerbate wildfire risks, especially when coupled with increases in overall growth rates from warmer temperatures.</p>
-             </div>
-            },
-            {name: <ForestLink changes={["ma::extreme weather::"]}>Extreme Weather</ForestLink>,
-             id: 'impacts_extreme_weather',
-             content: <div>
-               <p>Massachusetts is likely to experience a greater intensity and frequency of forest-disturbing weather events, including ice storms, localized or regional wind events such as microbursts or hurricanes, and severe thunderstorms. Forests will be especially vulnerable to wind and ice damage when they lack structural diversity from different stages of succession or forest growth.  Gaps in forest cover—whether caused by storm damage or other factors like fragmentation—will open up space for invasive species to move in and further compromise forest health.</p>
-             </div>
-            },
-            {name: <ForestLink changes={["ma::sea level rise::"]}>Sea Level Rise</ForestLink>,
-             id: 'impacts_sea_level_rise',
-             content: <div>
-               <p>As sea levels rise in Massachusetts and intensify the impacts of coastal storms and storm surge, forests near the shore may face greater risk of serious storm damage that can cause other cascading effects. Saltwater intrusion into groundwater will also elevate risks for coastal forest areas with significant tree mortality likely from salt poisoning.</p>
-             </div>
-            }]
+            }
+            ]
           },
-          {title: "Actions",
+          {title: "Headline",
            id: "actions",
            content: <p>
            </p>,
            subsections: [
-            {name: <ForestLink actions={["ma::planning::"]}>Planning</ForestLink>,
+            {name: <ForestLink actions={["ma::planning::"]}>Headline</ForestLink>,
              id:"impacts_planning",
              content: <ul>
-                <li>Complete a vulnerability assessment and develop a climate change adaptation plan that considers forest health and resilience.</li>
-                <li>Expand technical assistance and forest stewardship planning and practice programs for private forest owners.</li>
-                <li>Incorporate climate change, impacts on forests and strategies for keeping them healthy into town plans, open space plans, master plans and hazard mitigation plans.</li>
+                <li>Vader and a commander approach the troops as an Officer and </li>
+                <li>several heavily armed troops exit the spacecraft.</li>
              </ul>
             },
-            {name: <ForestLink q="management practices">Management Practices</ForestLink>,
+            {name: <ForestLink q="">Headline</ForestLink>,
              id: 'impacts_management',
              content: <ul>
-                <li>Continue active forest management for renewable wood products and wildlife benefits, and establish forest reserves to mitigate and monitor impacts of human development.</li>
-                <li>Increase forest diversity (species, structure, age classes and habitats) and vigor via professional forest management.</li>
-                <li>Use permanent land conservation tools to protect the most intact, productive and resilient forest ecosystems.</li>
-                <li>Expand the management of invasive species via a comprehensive strategy and a menu of programs to reach private and public owners.</li>
-                <li>Increase the percentage of wood products produced locally to strengthen markets and keep working forest landscapes economically viable.</li>
-                <li>Practice prudent fire management.</li>
-                <li>Promote tree regeneration by controlling deer/moose impacts.</li>
-                <li>Restore native tree species.</li>
-                <li>Return the forest to full stocking to increase capacity to sequester carbon.</li>
+                <li>Boy, it's lucky you had these compartments.</li>
+                <li>Han shakes his head, muttering to himself. Chewbacca agrees</li>
              </ul>
-            },
-            {name: <ForestLink actions={["ma::implementation action/direct action on target::technology"]}>Technology</ForestLink>,
-             id: 'impacts_technology',
-             content: <ul>
-                <li>Diversify urban forests via planting to increase carbon capture and resilience.</li>
-                <li>Promote drought and heat-adapted trees.</li>
-             </ul>
-            },
-            {name: <ForestLink actions={["ma::planning::policies/laws/regulations"]}>Policies / Laws</ForestLink>,
-             id: 'impacts_policy_law',
-             content: <ul>
-               <li>Consider policies that maintain forests, including local zoning regulations.</li>
-               <li>Support, via policies, sustainable management of renewable wood products on appropriate sites.</li>
-               <li>Establish and support initiatives to promote the buying of local forest products.</li>
-               <li>Encourage forest reserve management to allow natural processes to determine the long-term. structure, composition, function, and dynamics of the forest to the maximum extent possible.</li>
-             </ul>
-            },
-            {name: <ForestLink q="research monitoring">Research/Monitoring</ForestLink>,
-             id: 'impacts_research_monitoring',
-             content: <ul>
-               <li>Support long-term ecological monitoring programs such as the DCR‘s Continuous Forest Inventory.</li>
-               <li>Expand adaptive research of forest management practices and their effectiveness in a changing climate.</li>
-             </ul>
-            },
-            {name: <ForestLink actions={["ma::implementation action/direct action on target::financing"]}>Funding</ForestLink>,
-             id: 'impacts_funding',
-             content: <ul>
-                <li><a href='http://masswoods.net/landowner-programs'> Landowner programs for forest conservation. </a></li>
-                <li><a href='https://www.mass.gov/service-details/forest-stewardship-program'>    Forest Stewardship programs </a></li>
-                <li><a href='https://www.mass.gov/service-details/urban-and-community-forestry'>  Urban Forestry </a></li>
-             </ul>
-            },
+            }
            ]
           },
           {title: "Looking for help?",
            id: 'looking_for_help',
            subsections: [],
            content: <ul>
-             <li><SearchLink params={{query:'Massachusetts Division of Fisheries & Wildlife'}}>Massachusetts Division of Fisheries & Wildlife </SearchLink></li>
-             <li><SearchLink params={{query:'Massachusetts Department of Conservation and Recreation'}}>Massachusetts Department of Conservation and Recreation</SearchLink></li>
-             <li><SearchLink params={{query:'MassWoods'}}>MassWoods</SearchLink></li>
-             <li><SearchLink params={{query:'Nature Conservancy'}}>The Nature Conservancy</SearchLink></li>
-             <li><SearchLink params={{query:'USDA Forest Service Climate Change Resource Center'}}>USDA Forest Service Climate Change Resource Center</SearchLink></li>
+             <li><SearchLink params={{query:''}}>Headline </SearchLink></li>
+             
            </ul>
           }
         ]}
         >
-        <p>Massachusetts is 62 percent forested<Footnote num='1'/> and over 90 percent of that is upland forest<Footnote num='2'/>.  Upland forests support a variety of habitats and wide-ranging biological diversity. Forests also provide important filters along wetlands, rivers, and streams and stabilize soils and sediments, minimizing erosion. By providing shade to small streams, forests help moderate temperatures in aquatic habitats and provide shelter for wildlife in winter months. Forests play an important role in reducing greenhouse gas emissions because they take up and store vast amounts of carbon.</p>
-        <p>Each acre of forest in Massachusetts Is estimated to provide $1,500 annually<Footnote num='3'/> in economic value from forest products, water filtration, flood control, and tourism. Across the state’s 3.1 million acres of forest, that’s a total of $4.6 billion annually. Forest harvesting directly supports 3,700 jobs for foresters, loggers, sawmill workers, and wood processing plant workers in Massachusetts.</p>
-        <p>As we prepare for a changing climate, it is critically important that we protect forests and manage them with practices that promote their health and resilience. Warmer temperatures, more frequent extreme storms, intermittent droughts, and larger populations of forest pests that are expected in Massachusetts will create vulnerabilities for our forests in the decades ahead. Climate change may reduce forest health, alter species composition and forest structure, and affect many species of wildlife and ecosystem  services delivered by the forest such as flood protection.</p>
-        <p>Massachusetts is globally unique in the combination of population and forest density – it has more people living among more trees than most places on Earth!  However, there are still many urban areas with few trees nearby. Increasing urban forest canopies from their current average of about 20% to 40% would decrease carbon emissions (by reducing energy needs in summer and winter and by sequestering more carbon), reduce stormwater, increase urban air quality, address heat island effects, and improve human health.</p>
+        <p>TX-four-one-two. Why aren't you at your post? TX-four-one-two, do you copy?</p>
 
         </SectorDetailLayout>
     );
