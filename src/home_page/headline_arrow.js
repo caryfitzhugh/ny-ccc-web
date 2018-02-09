62429206dc015cf48a226f6891464532cffc28b1
Chris Diego <chrisdiego1@gmail.com>
2018-02-09 00:30:14 -0500

    responsive updates

--- src/home_page/headline_arrow.js
+++ src/home_page/headline_arrow.js
@@ -4,22 +4,20 @@ import {Link} from 'react-router-dom';
 import './headline_arrow.css';
 
 const HeadlineArrow = (props) => (
-	<div className="headline-arrow container">
-		<div className='row'>
-			<div className="col-12">
-			    <h2 className="home arrow">{props.title}&nbsp;  
-			     {props.showMore ?
-			      <small> 
-			        <a href={props.href}>
-			          More 
-			          <i className="fa fa-angle-double-right"></i>
-			        </a>
-			      </small> :
-			      null
-			  	  }
-			    </h2>
-		    </div>
-		 </div>
+	<div className="headline-arrow d-inline-block">
+		<div className="d-inline-block">
+		    <h2 className="home arrow">{props.title}&nbsp;  
+		     {props.showMore ?
+		      <small> 
+		        <a href={props.href}>
+		          More 
+		          <i className="fa fa-angle-double-right"></i>
+		        </a>
+		      </small> :
+		      null
+		  	  }
+		    </h2>
+	    </div>
 	</div>
 )
 
