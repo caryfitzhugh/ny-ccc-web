6519c9b22a0991a55dd582c28bfc84805009ddbe
Eric Murray <murray.m.eric@gmail.com>
2018-04-25 22:54:21 -0400

    updated footer

--- src/footer.js
+++ src/footer.js
@@ -43,12 +43,12 @@ class Footer extends Component {
             </div>
           </div>
           <div className='content col-md-4 col-xs-12'>
-            <h5>About NYCCSC</h5>
+            <h3>About NYCCSC</h3>
             <p className='hidden-md-down'>
               The New York Climate Change Science Clearinghouse (NYCCSC) is a regional gateway to data and information relevant to climate change adaptation and mitigation across New York State. It provides climate science data and literature and other resources for policy-makers, practitioners, and the public, to support scientifically sound and cost-effective decision making.
               <Link to='/about'> For More Information <i className="fa fa-angle-double-right"></i> </Link>
             </p>
-            <h5>Content Suggestions</h5>
+           <h3>Content Suggestions</h3>
             <p className='hidden-md-down'>
               We want to hear from you!  If you have ideas for datasets, data products, GIS layers, websites, documents, or other resources that would add value to the New York Climate Change Science Clearinghouse, please submit them. Content proposals are moderated, and submission of an idea is not a guarantee of inclusion in the Clearinghouse.
             </p>
@@ -57,7 +57,7 @@ class Footer extends Component {
             </Link>
           </div>
           <div className='content col-md-2 col-xs-12'>
-            <h5>Contact</h5>
+            <h3>Contact</h3>
             <p>
             Email: <a href='mailto:info@nyclimatescience.org'>info@nyclimatescience.org</a>
             <p>Phone: 607.273.6623 ext 22</p>
