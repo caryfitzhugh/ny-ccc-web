2bc6da054bae7a0aa98d77a635297c2b7edb0115
Chris Diego <chrisdiego1@gmail.com>
2018-04-26 21:44:19 -0400

    sector page

--- src/sectors/buildings.js
+++ src/sectors/buildings.js
@@ -49,42 +49,47 @@ Building adaptation strategies include improving building codes to promote sto
             id: 'preparing',
             subsections: [
               { name:"Planning",
-                id: "actions_planning",
+                id: "preparing_planning",
                 content: <ul>
+                  <li>Consider climate change projections when siting new buildings and facilities  </li>
+                  <li>Assess when to flood-proof vulnerable buildings, and when to relocate those structures  </li>
+                  <li>Plan micro-grids at critical facilities like schools and hospitals to provide energy for sheltering people during disasters  </li>
+                  <li>Strengthen and raise sea walls, and evaluate opportunities to use soft shoreline and bank armoring as an alternative to hard infrastructure where possible  </li>
+                  <SearchLink params={{query: "buildings planning"}}>> Find more information</SearchLink>
                 </ul>
               },
               { name: "Management Practices",
-                id: 'actions_management_practices',
+                id: 'preparing_management_practices',
                 content: <ul>
-                </ul>
-              },
-              { name: "Outreach / Education",
-                id: "actions_outreach",
-                content: <ul>
+                  <li>Formulate risk-based methods to evaluate the service life of flood control assets like sea walls and levees under changing climate conditions  </li>
+                  <li>Consider increasing the frequency of inspections for flood control structures  </li>
+                  <li>Install energy efficiency measures and use renewable energy  </li>
+                  <li>Consider purchasing appropriately-sized generators and pumps to handle flooding  </li>
+                  <li>Educate property owners and businesses about how to safely store chemicals and avoid spills during floods through safe storage  </li>
+                  <SearchLink params={{query: 'buildings management'}}>> Find more information</SearchLink>
                 </ul>
               },
               { name: "Technology",
-                id: "actions_tech",
+                id: "actions_outreach",
                 content: <ul>
-                <li>
-                  <ul>
-                  </ul>
-                </li>
+                  <li>Locate critical building systems and any toxics storage on higher stories or rooftops  </li>
+                  <li>Use Smart-Grid meters in buildings to manage demand spikes and prevent outages  </li>
+                  <SearchLink params={{query: 'buildings technology'}}>> Find more information</SearchLink>
                 </ul>
               },
               { name: "Policies / Laws",
                 id: "actions_policies_laws",
                 content: <ul>
+                  <li>Examine policies and regulations such as the <a href="https://www.dec.ny.gov/energy/102559.html">Community Risk and Resilience Act</a> to encourage climate-considered siting and design for buildings and infrastructure. </li>
+                  <li>Adjust local zoning regulations to incorporate flood protection for buildings  </li>
+                  <SearchLink params={{query: 'buildings policy law'}}>> Find more information</SearchLink>
                 </ul>
               },
               { name: "Research / Monitoring",
                 id: "actions_research",
                 content: <ul>
-                </ul>
-              },
-              { name: "Funding",
-                id: "actions_funding",
-                content: <ul>
+                  <li>Test and evaluate the effectiveness of green infrastructure like green roofs for cooling buildings and reducing runoff, and permeable pavement for infiltrating stormwater </li>
+                  <SearchLink params={{query: 'buildings research monitoring'}}>> Find more information</SearchLink>
                 </ul>
               }
             ]},
