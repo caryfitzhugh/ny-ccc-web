c44dc1515169a3806a15aa7a3a36bfe0772d78d5
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-04-28 11:00:22 -0400

    updated

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+if [[ -n $(git status --porcelain) ]]; then echo "repo is not checked in fully. Try again."; exit 1; fi
+rm -rf build/*
+npm run build
+
+aws s3 sync --profile=nescaum build/ s3://ny-web-nescaum-ccsc-dataservices/ --acl public-read
+
+aws cloudfront create-invalidation --profile=nescaum --distribution-id E20SBTJ4N03NL4 --paths "/*"
+rm -rf build/*
