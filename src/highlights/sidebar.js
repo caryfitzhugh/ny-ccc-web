a25cf8904b3a40bb7a037fcccee705fae8fa0234
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-05-01 18:07:41 -0400

    updates highlight sidebar links

--- src/highlights/sidebar.js
+++ src/highlights/sidebar.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import Collection from '../layouts/collection';
+import datagrapher_image from "../images/highlights/datagrapher.jpg";
+import map_image from "../images/highlights/map.png";
+import ccs_flood_exposure from "../images/highlights/ccs_flood_exposure.jpg";
+import climate_data_snapshots from "../images/highlights/climate_data_snapshots.jpg";
 
 
 import './sidebar.css';
+const sidebar_links = [
+  {  href: "/datagrapher",
+     title: "Climate Data Grapher",
+     image: datagrapher_image },
+  {  href: "/map",
+     title: "Interactive Map and GIS Viewer",
+     image: map_image },
+  {  href: "/resources/resources::1529",
+     title: "Coastal County Snapshots - Flood Exposure",
+     image: ccs_flood_exposure},
+  {  href: "/resources/resources::1512",
+     title: "Climate Data Snapshots",
+     image: climate_data_snapshots}
+];
 
 const Sidebar = (props) => (
   <div className="col-sm-3 sidebar hidden-xs">
@@ -42,7 +60,18 @@ const Sidebar = (props) => (
       <p>
         <strong>Use these interactive maps and charts to build your own data visualizations:</strong>
       </p>
-      <Collection collection_name={props.mapCollection} show_images={true} />
+      <ul className='sidebar-links'>
+          { (sidebar_links).map((link, i) => {
+              return (
+                <li className='sidebar-link' key={'sidebar-' + i}>
+                  <img src={link.image} />
+                  <a href={link.href}>
+                    {link.title}
+                  </a>
+                </li>
+              )})
+          }
+       </ul>
     </div>
   </div>
 )
