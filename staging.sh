dc443fc5adfc03f3f97b1924628656674649a8e0
Cary FitzHugh <cary.fitzhugh@gmail.com>
2018-06-07 17:54:39 -0400

    staging deploy for ny web script

--- staging.sh
+++ staging.sh
@@ -1,6 +1,6 @@
 rm -rf build/
 npm run build
 
-aws s3 sync --profile=nescaum build/ s3://ny-web-nescaum-ccsc-dataservices --acl public-read
+aws s3 sync --profile=nescaum build/ s3://ny-web-staging-nescaum-ccsc-dataservices --acl public-read
 rm -rf build/
-echo "Page deployed to: http://ny-web-nescaum-ccsc-dataservices.s3-website-us-east-1.amazonaws.com"
+echo "Page deployed to: http://ny-web-staging-nescaum-ccsc-dataservices.s3-website-us-east-1.amazonaws.com"
